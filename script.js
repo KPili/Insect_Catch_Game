bdf7bdd26268b7498e345ca04c5f66ae5ad93a86
Kurt <kurtpili3@gmail.com>
2021-02-16 18:10:26 -0600

    CSS done

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function load(){
 
+screenFocus();
+
+}
+
+function screenFocus(){
+    const screens = document.querySelectorAll('.screen');
+
+    screens[0].classList.add('up');
 }
 
 document.addEventListener("DOMContentLoaded", load);
